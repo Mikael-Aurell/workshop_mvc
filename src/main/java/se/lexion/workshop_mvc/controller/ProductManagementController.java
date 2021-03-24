1a779961d4177737576102b420b170d695eff648
Mikael Aurell <aurell.mikael@gmail.com>
2021-03-24 16:24:44 +0100

    Added productDetails.html

--- src/main/java/se/lexion/workshop_mvc/controller/ProductManagementController.java
+++ src/main/java/se/lexion/workshop_mvc/controller/ProductManagementController.java
@@ -3,6 +3,7 @@ package se.lexion.workshop_mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.lexion.workshop_mvc.dto.ProductDetailsDto;
 import se.lexion.workshop_mvc.dto.ProductDto;
@@ -20,6 +21,7 @@ public class ProductManagementController {
     @PostConstruct
     public void init() {
         if(productDtoList == null) productDtoList = new ArrayList<>();
+
         ProductDto productDto= new ProductDto();
         productDto.setId(1);
         productDto.setPrice(20);
@@ -46,12 +48,32 @@ public class ProductManagementController {
         productDto2.setProductDetailsDto(productDetailsDto2);
 
         productDtoList.add(productDto2);
+
+        ProductDto productDto3 = new ProductDto();
+        productDto3.setId(3);
+        productDto3.setPrice(50);
+        productDto3.setName("OCX Book");
+        ProductDetailsDto productDetailsDto3= new ProductDetailsDto();
+        productDetailsDto3.setId(1);
+        productDetailsDto3.setName("Test 3");
+        productDetailsDto3.setDescription("Test description 3");
+
+        productDto2.setProductDetailsDto(productDetailsDto2);
+
+        productDtoList.add(productDto2);
     }
 
     @GetMapping("/")
     public String getAllProducts(Model model) {
         model.addAttribute("productDtoList", productDtoList);
         return "productManagement";
+    }
+
+    @GetMapping("/find/{id}")
+    public String getProductById(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("getProduct",
+                productDtoList.stream().filter(productDto -> productDto.getId() == id).findFirst().get());
+        return "productDetails";
     }
 
 }
